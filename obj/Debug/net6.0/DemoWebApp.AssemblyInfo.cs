adff097a8cc28756e24600270aaabd89980e6691
Everbrighthub01 <everbrighthub@gmail.com>
2025-03-03 14:40:22 +0100

    Demo application

--- obj/Debug/net6.0/DemoWebApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/DemoWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f64b87ebf45b08b54fbff26e8a7d57128cff68d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
